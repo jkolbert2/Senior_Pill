b6b01cfcae9ef4d35edda5b0d6ee654c99269b33
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-16 22:31:50 +0000

    adds route for action for button for reminders

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   resources :reminders
   root :to => redirect('/reminders')
-  
+  get :reminder_email, to: 'reminder_mailer#reminder_email', as: :reminder_email
   
 end
 
