8327108bec20b3626ae7a24671a3297594e4323a
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-16 22:42:52 +0000

    fixes routes relative to remintder template connecting to method for button

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,7 +1,7 @@
 class ReminderMailer < ApplicationMailer
     default from: 'donotreply@pillproject.com'
     def reminder_email
-        @reminder = params[:Reminder]
+        @reminder = params[:reminder]
         @name = @reminder.Name 
         mail(to: @reminder.Email, subject: 'REMINDER')
         
