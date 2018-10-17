5ba002e94a3725b710e12ef87a3ab7de339029eb
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-17 04:40:23 +0000

    hardcoded test passwords to see if the problem is from variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'mail.google.com',
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
+  :user_name            => 'pillprojectteam@gmail.com',
+  :password             => 'PILLproject123',
   :authentication       => "login",
   :enable_starttls_auto => true,
   :openssl_verify_mode  => 'none'
