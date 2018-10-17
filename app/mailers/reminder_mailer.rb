65e0a28787ad647b5008cd260bc161624c8a486f
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-17 03:33:45 +0000

    changes email address

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,6 +1,6 @@
 class ReminderMailer < ApplicationMailer
     
-    default from: 'donotreply@pillproject.com'
+    default from: 'from@example.com'
     def reminder_email(reminder)
         @reminder = reminder
         mail(to: @reminder.Email, subject: 'REMINDER')
