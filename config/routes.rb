7a3e6af239934b5b2756faf4ca1766207a971d27
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-17 02:37:07 +0000

    typo fix and retrying older method - bc I missed the type before

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   resources :reminders
   root :to => redirect('/reminders')
-  get :send_remainder, to: 'reminders_controller#send_reminder', as: :send_reminder
+  get :send_reminder to: 'reminders_controller#send_reminder', as: :send_reminder
   
 end
 
