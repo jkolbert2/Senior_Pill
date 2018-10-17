6a5f20d8c918354ba61240752f4f2345622df1c6
Jordan Kolbert <jkolbert@tulane.edu>
2018-10-17 03:57:44 +0000

    changed authentication from plain to login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   :port                 => 587,
   :user_name            => ENV['gmail_username'],
   :password             => ENV['gmail_password'],
-  :authentication       => "plain",
+  :authentication       => "login",
   :enable_starttls_auto => true
 }
 
