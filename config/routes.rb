c709d5101c8cb7d5c5373bd0a2f43f9f94289cdf
Joseph Kim <jkim15@tulane.edu>
2018-10-18 01:29:15 +0000

    Add more updates to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,22 @@ Rails.application.routes.draw do
   
   resources :reminders
   root :to => redirect('/reminders')
+<<<<<<< HEAD
   get '/send_reminder/:id', to: 'reminders#send_reminder', as: :send_reminder
   
 end
 
+=======
+  get :send_reminder, to: 'send_reminder', as: :send_reminder
+>>>>>>> Add more updates to routes
   resource :messages do
     collection do
       post 'reply'
     end
   end
+<<<<<<< HEAD
+=======
+
+
+end
+>>>>>>> Add more updates to routes
