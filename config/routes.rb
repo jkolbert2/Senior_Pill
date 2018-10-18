5a04f395e97f913a0a7831eaf47be32124799360
Joseph Kim <jkim15@tulane.edu>
2018-10-18 01:25:16 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@
 Rails.application.routes.draw do
   resources :reminders
   root :to => redirect('/reminders')
-end
-
-Rails.application.routes.draw do
+ 
   resource :messages do
     collection do
       post 'reply'
