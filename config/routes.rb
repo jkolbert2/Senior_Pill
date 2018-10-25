4b6cb3212ed04b8a7eb5a38aff1b2eabb7565827
Joseph Kim <jkim15@tulane.edu>
2018-10-25 20:13:46 +0000

    Added Twilio, did not break

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,11 @@ Rails.application.routes.draw do
   
   resources :reminders
   root :to => redirect('/reminders')
-<<<<<<< HEAD
-  get '/send_reminder/:id', to: 'reminders#send_reminder', as: :send_reminder
-  
-end
-
-=======
   get :send_reminder, to: 'send_reminder', as: :send_reminder
->>>>>>> Add more updates to routes
+end
   resource :messages do
     collection do
       post 'reply'
-    end
-  end
-<<<<<<< HEAD
-=======
-
-
 end
->>>>>>> Add more updates to routes
+end
+
