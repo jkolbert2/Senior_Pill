535996dea45f95b0ecaab6b3fc3dab4a5ac3371d
Joseph Kim <jkim15@tulane.edu>
2018-10-28 17:48:22 +0000

    Work on routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   root :to => redirect('/reminders')
   get '/send_reminder/:id', to: 'reminders#send_reminder', as: :send_reminder
   
-
+  resource :messages do
+    collection do
+      post 'reply'
+    end
+  end
 
 
 end
