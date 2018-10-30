bc69058e18e1244c6e38ce88019fe5185ae9b496
Jacqueline Failla <jfailla1@tulane.edu>
2018-10-30 21:41:38 +0000

    Users Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   
   
+  resources :users
   resources :reminders
   root :to => redirect('/reminders')
   get '/send_reminder/:id', to: 'reminders#send_reminder', as: :send_reminder
