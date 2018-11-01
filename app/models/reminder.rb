70e9fcd355ee73b0a2c8d9dd9f618392d09beb7e
Jordan Kolbert <jkolbert@tulane.edu>
2018-11-01 18:52:07 +0000

    its a plural issue

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -6,7 +6,7 @@ class Reminder < ActiveRecord::Base
     validates_date :Date, on_or_before: lambda { Date.current }
     validates :Name, presence: true, length: { maximum: 255 } 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
+    validates :Email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
 uniqueness: { case_sensitive: false }
 end
