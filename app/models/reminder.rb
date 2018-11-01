7e9031a23716a9114f74b275fca7bbe6966d632a
Jordan Kolbert <jkolbert@tulane.edu>
2018-11-01 18:45:54 +0000

    email verification fix

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,8 +3,10 @@ class Reminder < ActiveRecord::Base
     belongs_to :user
     validates :user_id, presence: true
     default_scope -> { order(created_at: :desc) }
-    
-    validates :Name, presence: true
-    validates :Email, presence: true
-
+    validates_date :Date, on_or_before: lambda { Date.current }
+    validates :Name, presence: true, length: { maximum: 255 } 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+uniqueness: { case_sensitive: false }
 end
