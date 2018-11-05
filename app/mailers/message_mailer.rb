45d643a292cf20d5b8f85055d514a355ff661c3a
Caroline Hixon <chixon@tulane.edu>
2018-11-05 20:28:19 +0100

    aesthetic fixes.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,7 +3,8 @@ class MessageMailer < ApplicationMailer
 
   def contact_me(message)
     @body = message.body
-
+    
+      #email that messages send to
     mail to: "caroline@example.org", from: message.email
   end
 end
