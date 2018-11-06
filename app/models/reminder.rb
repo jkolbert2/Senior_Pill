b398b87a5bd68bcf5d8e1b8b1c54daedc536585e
Jordan Kolbert <jkolbert@tulane.edu>
2018-11-06 19:54:05 +0000

    reminder validtion deletion

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -5,8 +5,8 @@ class Reminder < ActiveRecord::Base
     default_scope -> { order(created_at: :desc) }
  #    validates_date :Date, on_or_before: lambda { Date.current }
     validates :Name, presence: true, length: { maximum: 255 } 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :Email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+ #   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :Email, presence: true, length: { maximum: 255 } #,
+    #                format: { with: VALID_EMAIL_REGEX },
+    #                uniqueness: { case_sensitive: false }
 end
