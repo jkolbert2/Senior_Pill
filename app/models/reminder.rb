939abd5e07816291774c31daefeee12e31367863
Jordan Kolbert <jkolbert@tulane.edu>
2018-11-06 19:49:11 +0000

    date error fix

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,7 +3,7 @@ class Reminder < ActiveRecord::Base
     belongs_to :user
     validates :user_id, presence: true
     default_scope -> { order(created_at: :desc) }
-    validates_date :Date, on_or_before: lambda { Date.current }
+ #    validates_date :Date, on_or_before: lambda { Date.current }
     validates :Name, presence: true, length: { maximum: 255 } 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :Email, presence: true, length: { maximum: 255 },
