f7fa8a61482aee9b01210041dd00a76f95bcd068
Jordan Kolbert <jkolbert@tulane.edu>
2018-11-06 18:08:43 +0000

    syntax fixes

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -8,5 +8,5 @@ class Reminder < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :Email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false }
 end
