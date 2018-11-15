acab6de8ae05328075e730a304e6463c7a07f0c2
Joseph Kim <jkim15@tulane.edu>
2018-11-15 18:22:35 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-<<<<<<< HEAD
-reminders = [
-    {:Date => '9/24/2018', :Name => 'Jackie', :Drug => 'Advil',:Purpose => 'Fever',:Amount => 2,:Email => 'reminderstest24@gmail.com'},
-    {:Date => '9/24/2018', :Name => 'Jackie', :Drug => 'Claritin',:Purpose => 'Fever',:Amount => 2, :Email => 'reminderstest24@gmail.com'}
-]
-    
-reminders.each do |drug|
-    Reminder.create!(drug)
-end
-25.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@pillProject.org"
-  User.create!(name:  name,
-               email: email)
-=======
+
 
 
 25.times do  
@@ -30,5 +16,5 @@ end
   user.password = '123456'
   user.password_confirmation = '123456'
   user.save
->>>>>>> accb87cc853136a34ad8193f1d24ee1f85faa58b
 end
+
