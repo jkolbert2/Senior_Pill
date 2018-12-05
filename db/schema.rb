56d8eb31f943f2e8e8ad4d85f11a0f292f47ca8e
Jordan Kolbert <jkolbert@tulane.edu>
2018-12-05 04:38:50 +0000

    added patientlist array to users -c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205031533) do
+ActiveRecord::Schema.define(version: 20181205041224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20181205031533) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.integer  "patientlist",                                  array: true
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
