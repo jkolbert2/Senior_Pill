b2213f3abf6c3b95a9dee769f95ccb6b36ec6daa
Caroline Hixon <chixon@tulane.edu>
2018-12-06 01:57:24 +0000

    setting patient list to null at acccoutn creation -j

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :patientlist => [])
   end
   
   def newpatient_params
