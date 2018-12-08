e0cc953227ef11ced1f53fea52d0091d11f9fd62
Jordan Kolbert <jkolbert@tulane.edu>
2018-12-08 21:04:42 +0000

    adds doctors to reminder field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205232817) do
+ActiveRecord::Schema.define(version: 20181206192223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20181205232817) do
     t.integer  "user_id"
     t.string   "frequency"
     t.boolean  "Active"
+    t.string   "doctor"
   end
 
   create_table "sessions", force: :cascade do |t|
