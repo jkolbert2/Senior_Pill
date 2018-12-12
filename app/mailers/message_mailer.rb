ed71df95fd6f8425dc058b4fe6dd55d9569385e3
Caroline Hixon <chixon@tulane.edu>
2018-12-12 06:27:29 +0000

    Added subject in en.yml

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,13 +1,9 @@
 class MessageMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.message_mailer.contact_me.subject
-  #
-  def contact_me
+  def contact_me(message)
     @greeting = "Hi"
 
     mail to: "to@example.org"
   end
+
 end
