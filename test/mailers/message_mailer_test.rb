a2fb37281091e357b67c3478960fc6320b6e2f2a
Caroline Hixon <chixon@tulane.edu>
2018-12-12 06:26:06 +0000

    added mailer testing.

--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -2,11 +2,19 @@ require 'test_helper'
 
 class MessageMailerTest < ActionMailer::TestCase
   test "contact_me" do
-    mail = MessageMailer.contact_me
-    assert_equal "Contact me", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+    message = Message.new name: 'anna',
+                          email: 'anna@example.org',
+                          body: 'hello, how are you doing?'
 
+    email = MessageMailer.contact_me(message)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal 'Message from www.murdo.ch', email.subject
+    assert_equal ['stephen@example.org'], email.to
+    assert_equal ['anna@example.org'], email.from
+    assert_match /hello, how are you doing?/, email.body.encoded
+  end
 end
