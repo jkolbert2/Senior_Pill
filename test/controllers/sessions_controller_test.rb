a59e26df95b8172c367738099909fb933a8591d4
Joseph Kim <jkim15@tulane.edu>
2018-12-14 22:43:23 +0000

    Bug fixing

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,10 +1,10 @@
-#require 'test_helper'
+require 'test_helper'
 
-#class SessionsControllerTest < ActionDispatch::IntegrationTest
-  #test "should get new" do
-   # get sessions_new_url
-  #  assert_response :success
- # end
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get sessions_new_url
+    assert_response :success
+  end
 
-#end
+end
 
