a0e15bfdea4376c337e676f72ced176719bc42e5
Caroline Hixon <chixon@tulane.edu>
2018-12-15 18:18:45 +0000

    fix contact

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,16 +3,17 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
   def create
-    @message = Message.new message_params
+    @message = Message.new(message_params)
+
     if @message.valid?
       MessageMailer.contact_me(@message).deliver_now
-      redirect_to new_message_url
-      flash[:notice] = "We have received your message and we will be in touch soon!"
+      redirect_to messages_new_url, notice: "Message received"
     else
-      flash[:notice] = "There was an error sending your message. Please try again!"
       render :new
     end
   end
+  
+
 private
     def message_params
     params.require(:message).permit(:name, :email, :body)
--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -1,9 +1,12 @@
-# Preview all emails at http://localhost:3000/rails/mailers/message_mailer
 class MessageMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/message_mailer/contact_me
-  def contact_me
-    MessageMailer.contact_me
+  def contact_me 
+    message = Message.new name: 'marflar', 
+                          email: 'marflar@example.org',
+                          body: 'This is the body of the email'
+
+    MessageMailer.contact_me message
   end
 
 end
